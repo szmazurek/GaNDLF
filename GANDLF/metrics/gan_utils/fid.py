b82f7be4732dd028c0430321724a235bda4339c8
Szymon Mazurek <48680634+szmazurek@users.noreply.github.com>
2024-03-18 22:38:38 +0100

    Update GANDLF/metrics/gan_utils/fid.py
    Co-authored-by: Sarthak Pati <patis@iu.edu>

--- GANDLF/metrics/gan_utils/fid.py
+++ GANDLF/metrics/gan_utils/fid.py
@@ -166,9 +166,8 @@ class NoTrainInceptionV3(_FeatureExtractorInceptionV3):
 
 
 def _compute_fid(mu1: Tensor, sigma1: Tensor, mu2: Tensor, sigma2: Tensor) -> Tensor:
-    r"""Compute adjusted version of `Fid Score`_.
-
-    The Frechet Inception Distance between two multivariate Gaussians X_x ~ N(mu_1, sigm_1)
+    """
+    This function calculates the adjusted version of `Fid Score`_. The Frechet Inception Distance between two multivariate Gaussians X_x ~ N(mu_1, sigm_1)
     and X_y ~ N(mu_2, sigm_2) is d^2 = ||mu_1 - mu_2||^2 + Tr(sigm_1 + sigm_2 - 2*sqrt(sigm_1*sigm_2)).
 
     Args:
