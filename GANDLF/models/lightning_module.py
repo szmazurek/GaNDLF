1cc8f42222675c8b10b01d8c13125b987301346e
szmazurek <szymon.mazurek@cyfronet.pl>
2024-11-09 15:08:25 +0100

    Adapt lightning module to new functionalities

--- GANDLF/models/lightning_module.py
+++ GANDLF/models/lightning_module.py
@@ -3,7 +3,8 @@ from GANDLF.models import get_model
 from GANDLF.optimizers import get_optimizer
 from GANDLF.schedulers import get_scheduler
 from GANDLF.losses.loss_calculators import LossCalculatorFactory
-from GANDLF.models.modelBase import ModelBase
+from GANDLF.metrics.metric_calculators import MetricCalculatorFactory
+from GANDLF.utils.pred_target_processors import PredictionTargetProcessorFactory
 
 from copy import deepcopy
 
@@ -12,8 +13,27 @@ class GandlfLightningModule(pl.LightningModule):
     def __init__(self, params: dict):
         super().__init__()
         self.params = deepcopy(params)
-        self.model: ModelBase = get_model(params)
-        self.loss = LossCalculatorFactory(params).get_loss_calculator()
+        self._initialize_model()
+        self._initialize_loss()
+        self._initialize_metric_calculators()
+        self._initialize_preds_target_processor()
+
+    def _initialize_model(self):
+        self.model = get_model(self.params)
+
+    def _initialize_loss(self):
+        self.loss = LossCalculatorFactory(self.params).get_loss_calculator()
+
+    def _initialize_metric_calculators(self):
+        # TODO can we have situation that metrics are empty?
+        self.metric_calculators = MetricCalculatorFactory(
+            self.params
+        ).get_metric_calculator()
+
+    def _initialize_preds_target_processor(self):
+        self.pred_target_processor = PredictionTargetProcessorFactory(
+            self.params
+        ).get_prediction_target_processor()
 
     def configure_optimizers(self):
         params = deepcopy(self.params)
